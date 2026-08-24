a66cc200ab99f3d67dd38992114217407762ef9c
jeniichee <cheung.jenn@northeastern.edu>
2026-08-23 23:14:01 -0400

    actually added the wallpaper this time

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // resolve: {
+  //   alias: {
+  //     ".components": resolve(
+  //       dirname(fileURLToPath(import.meta.url)),
+  //       "src/components",
+  //     )
+  //   }
+  // }
 })

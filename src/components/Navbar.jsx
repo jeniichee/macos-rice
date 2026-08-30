6147688bf038253ecd421ebced214574ea706157
jeniichee <cheung.jenn@northeastern.edu>
2026-08-29 22:32:43 -0400

    added time

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,58 @@
-import React from 'react';
-import { navIcons, navLinks } from '../constants';
-// import dayjs from "dayjs";
-
+import React, { useEffect } from "react";
+import { navIcons, navLinks } from "../constants";
 
 export const Navbar = () => {
-    return (
-      <nav>
-        <div>
-          <img src="/images/logo.svg" />
-          <p className="font-bold">Jen's Portfolio</p>
 
-          <ul>
-            {navLinks.map(({ id, name, type }) => (
+  function updateTime() {
+    const date = new Date(); 
+    let hour = date.getHours(); 
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    hour = hour < 10 ? "0" + hour : hour;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    const time_string = `${hour}:${minutes}:${seconds}`;
+    document.getElementById("time").textContent = time_string; 
+  }
+
+  useEffect(() => {
+    updateTime();
+
+    const interval = setInterval(updateTime, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <nav>
+      <div>
+        <img src="/icons/logo.svg" />
+        <p className="font-bold">Jen's Portfolio</p>
+
+        <ul>
+          {navLinks.map(({ id, name, type }) => (
             //   <li key={id} onClick={() => openWindow(type)}>
-                <p>{name}</p>
-              /* </li> */
-            ))}
-          </ul>
-        </div>
+            <p>{name}</p>
+            /* </li> */
+          ))}
+        </ul>
+      </div>
 
-        <div>
-          <ul>
-            {navIcons.map(({ id, img }) => (
-              <li key={id}>
-                <img src={img} alt={"icon-${id}"} className="icon" />
-              </li>
-            ))}
-          </ul>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} alt={`icon-${id}`} className="icon" />
+            </li>
+          ))}
+        </ul>
 
-          {/* <time>{dayjs().format("ddd MMM D h:mm A")}</time> */}
-        </div>
-      </nav>
-    );
-}
+        <time id="time">00:00:00</time>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar; 
+export default Navbar;
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -8,6 +8,5 @@ export const navLinks = [
 export const navIcons = [
   { id: 1, img: "/icons/wifi.svg" },
   { id: 2, img: "/icons/search.svg" },
-  { id: 3, img: "/icons/user.svg" },
   { id: 4, img: "/icons/mode.svg" },
 ];
